e78100dacb8a1ce5d4418ad735a919975dc6149f
paramsd1997 <111410267+paramsd1997@users.noreply.github.com>
2023-03-21 13:25:31 +0530

    Create set_html.ps1

---
+++ set_html.ps1
@@ -0,0 +1,11 @@
+Set-Content -Path "C:\Path\To\Your\File.html" -Value @"
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+</body>
+</html>
+"@
